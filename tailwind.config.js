04a5eeed1e4c6b100d0f32dc01c9d200d8df5e51
shuntatakemoto <rt80405@gmail.com>
2021-10-16 18:48:54 +0900

    feat:メインカラーとサブカラーを設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  mode: 'jit',
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: { colors: { 'main-color': '#FFFFF0', 'sub-color': '#EEE8AA' } },
+    fontFamily: {
+      sans: ['Nunito Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+    boxShadow: {
+      inner: 'rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset',
+    },
   },
   variants: {
     extend: {},
