29ec5ec2cccc97e05ccc5e03fc7a214578a1bf0c
shuntatakemoto <rt80405@gmail.com>
2021-10-24 23:25:58 +0900

    feat:Redux toolkit導入

---
+++ src/app/store.ts
@@ -0,0 +1,16 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import userReducer from '../features/slices/userSlices';
+
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
---
+++ src/features/slices/userSlices.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: { uid: '', photoUrl: '', displayName: '', twitterUid: '' },
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = { uid: '', photoUrl: '', displayName: '', twitterUid: '' };
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+export const selectUser = (state: RootState) => state.user.user;
+
+export default userSlice.reducer;
