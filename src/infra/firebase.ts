ca1a21f9266d16808528b0c3055dd423f8d5ecdd
shuntatakemoto <rt80405@gmail.com>
2021-11-07 20:44:29 +0900

    feat: Firebaseを導入

---
+++ src/infra/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+export const storage = firebase.storage();
+export const provider = new firebase.auth.TwitterAuthProvider();
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import React from 'react';
+import { auth, provider } from '../infra/firebase';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const Home: NextPage = () => {
+  const signInTwitter = async () => {
+    await auth.signInWithPopup(provider).catch((err: any) => alert(err.message));
+  };
+
   return (
     <div className=''>
       <Head>
@@ -11,7 +19,59 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className=''></main>
+      <main className='text-center bg-main-color'>
+        <div className=''>
+          <p className='text-5xl p-10 font-bold'>Food Invite</p>
+          <p className='m-5'>サービスを利用するにはTwitterでログインが必要です。</p>
+          <button className='bg-sub-color p-2 my-5 rounded-2xl text-center' onClick={signInTwitter}>
+            <LockOpenIcon />
+            &nbsp;Sign In with Twitter
+          </button>
+          <p className='text-2xl p-5 mt-5'>このアプリは何？</p>
+          <p>・知り合いをご飯に誘うハードルを下げること </p>
+          <p>・地元民がお勧めする飲食店を知ること</p>
+          <p>を目的としたアプリです</p>
+          {/* <p>具体的には、以下の自分の経験を解決するために開発しました</p>
+        <p>・あまり親交はないが一緒にご飯に行って話を聞きたいという経験</p>
+        <p>
+          ・旅行などで外食の際、食○ログで上位の店に行くのではなく、旅行先の地元民がお勧めする店に行きたいという経験
+        </p>
+        <p>・友達とご飯に行く際、食べに行く店がなかなか決まらなかった経験</p> */}
+          <div className=' p-5 '>
+            <p className='text-2xl mt-10'>このアプリでできることは？</p>
+            <p className=' mt-5 '>
+              ・行きたい飲食店リスト、お気に入り飲食店リストなどといったリストを作成できます（イメージはAmazonの欲しいものリスト）
+            </p>
+            <br />
+            <p>
+              ・共同編集でみんなでリストを作成できます（例えば鹿児島旅行のときに外せないとんかつ屋さんのリストに対して店追加のリクエストができる）
+            </p>
+            <br />
+            <p>
+              ・Twitterで今日のご飯行く相手の募集ができます（自分の行きたい店リストをtwitterに共有し、行きたい人がいたらDMに通知される）
+            </p>
+            <br />
+            <p>
+              ・都道府県ごとにリスト、お店を検索できます（旅行先のご飯を決めるときはFood
+              Inviteで検索！）
+            </p>
+            <br />
+            <p></p>
+          </div>
+          <p className='mt-10 mb-5'>ソースコード⬇︎</p>
+          <a href='https://github.com/shuntatakemoto/food-invite'>
+            <button className='bg-sub-color p-2  rounded-2xl text-center'>
+              <GitHubIcon />
+              &nbsp;Githubを開く
+            </button>
+          </a>
+          <p className='mt-10'>
+            作成者:
+            <a href='https://twitter.com/haruta_8_'>&nbsp;@haruta_8_</a>
+          </p>
+          <p></p>
+        </div>
+      </main>
     </div>
   );
 };
