1196276b6f0c2fb71a5ca6181ae63bd5bdd58171
shuntatakemoto <rt80405@gmail.com>
2021-11-27 20:09:52 +0900

    feat: 検索ページ追加

---
+++ src/pages/search.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Search: React.FC = () => {
+  return (
+    <main className='flex flex-col bg-main-color'>
+      <p className='text-center font-bold text-2xl'>現在開発中です</p>
+    </main>
+  );
+};
+
+export default Search;
