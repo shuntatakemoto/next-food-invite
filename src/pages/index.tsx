daff066162b3aec0d23722f96c657032ec94321d
shuntatakemoto <rt80405@gmail.com>
2021-12-21 18:48:03 +0900

    docs: デザインの微調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,22 +23,17 @@ const Home: NextPage = () => {
 
       <Layout>
         <main className='text-center bg-main-color'>
-          <p className='text-5xl p-10 font-bold'>Food Invite</p>
-          <p className='m-5'>サービスを利用するにはTwitterでログインが必要です。</p>
-          <Button label='Sign In with Twitter' onClick={signInTwitter} />
-          <p className='text-2xl p-5 mt-5'>このアプリは何？</p>
+          <p className='text-5xl p-12 font-bold'>Food Invite</p>
+          <p className='m-4'>サービスを利用するにはTwitterでログインが必要です。</p>
+          <Button label='Sign In with Twitter' onClick={signInTwitter} backgroundColor='#EEE8AA' />
+          <p className='text-2xl mt-16'>このアプリは何？</p>
           <p>・知り合いをご飯に誘うハードルを下げること </p>
           <p>・地元民がお勧めする飲食店を知ること</p>
           <p>を目的としたアプリです</p>
-          {/* <p>具体的には、以下の自分の経験を解決するために開発しました</p>
-        <p>・あまり親交はないが一緒にご飯に行って話を聞きたいという経験</p>
-        <p>
-          ・旅行などで外食の際、食○ログで上位の店に行くのではなく、旅行先の地元民がお勧めする店に行きたいという経験
-        </p>
-        <p>・友達とご飯に行く際、食べに行く店がなかなか決まらなかった経験</p> */}
-          <div className=' p-5 '>
-            <p className='text-2xl mt-10'>このアプリでできることは？</p>
-            <p className=' mt-5 '>
+
+          <div className=' p-4 '>
+            <p className='text-2xl mt-12'>このアプリでできることは？</p>
+            <p className=' mt-4'>
               ・行きたい飲食店リスト、お気に入り飲食店リストなどといったリストを作成できます（イメージはAmazonの欲しいものリスト）
             </p>
             <br />
@@ -55,11 +50,11 @@ const Home: NextPage = () => {
               Inviteで検索！）
             </p>
           </div>
-          <p className='mt-10 mb-5'>ソースコード⬇︎</p>
+          <p className='pt-12 pb-4'>ソースコード⬇︎</p>
           <a href='https://github.com/shuntatakemoto/next-food-invite'>
             <Button label='Githubを開く' />
           </a>
-          <p className='mt-10'>
+          <p className='pt-12 pb-4'>
             作成者:
             <a href='https://twitter.com/haruta_8_'>&nbsp;@haruta_8_</a>
           </p>
