a1c872b038a4eee7feb0934707c418f5d82cf5a4
shuntatakemoto <rt80405@gmail.com>
2021-12-21 16:18:02 +0900

    feat: ブックマークの削除ボタンの位置とロジック変更

--- src/components/molecules/FullPost/index.tsx
+++ src/components/molecules/FullPost/index.tsx
@@ -38,25 +38,34 @@ const FullPost: React.FC<FullPostProps> = (props) => {
 
   return (
     <div className=' rounded-lg shadow-xl overflow-hidden h-48 xl:h-60 m-4 xl:m-6'>
-      <Link
-        href={
-          router.pathname.includes('bookmark')
-            ? `${props.listurl}`
-            : `/users/${props.uid}/lists/${props.listId}`
-        }
-      >
-        <div className='h-36 xl:h-48 grid justify-items-center items-center bg-gray-200'>
-          <Emoji emoji={emojiName} size={64} set='twitter' />
-        </div>
-      </Link>
-      <div className='text-center h-12 xl:h-12 grid justify-items-center items-center bg-gray-200'>
-        <p className='text-base'>{props.listname}</p>
-        {router.pathname.includes('bookmark') && (
-          <button onClick={deleteBookmark}>
-            <HighlightOffIcon />
-          </button>
-        )}
-      </div>
+      {router.pathname.includes('bookmark') ? (
+        <Link href={`${props.listurl}`} passHref>
+          <div className='bg-gray-200'>
+            <button onClick={deleteBookmark} className='w-full h-8 grid justify-end items-center'>
+              <HighlightOffIcon />
+            </button>
+            <div className='h-28 xl:h-40 grid justify-items-center items-center '>
+              <Emoji emoji={emojiName} size={64} set='twitter' />
+            </div>
+
+            <div className='text-center h-12 xl:h-12 grid justify-items-center items-center'>
+              <p className='text-base'>{props.listname}</p>
+            </div>
+          </div>
+        </Link>
+      ) : (
+        <Link href={`/users/${props.uid}/lists/${props.listId}`} passHref>
+          <div className='bg-gray-200'>
+            <div className='h-36 xl:h-48 grid justify-items-center items-center'>
+              <Emoji emoji={emojiName} size={64} set='twitter' />
+            </div>
+
+            <div className='text-center h-12 xl:h-12 grid justify-items-center items-center'>
+              <p className='text-base'>{props.listname}</p>
+            </div>
+          </div>
+        </Link>
+      )}
     </div>
   );
 };
