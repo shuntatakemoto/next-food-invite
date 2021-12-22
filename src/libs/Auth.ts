71b1d697deae2fb4957357c8c12eee5a8018a6c1
shuntatakemoto <rt80405@gmail.com>
2021-12-22 18:55:59 +0900

    fix: 保存する条件の修正

--- src/libs/Auth.ts
+++ src/libs/Auth.ts
@@ -1,25 +1,14 @@
 import firebase from 'firebase/app';
-import router from 'next/router';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useJudgeLogin } from '../hooks/useJudgeLogin';
 import { login, logout, selectUser } from '../store/user';
-import { Params } from '../types/params';
 import { auth, db } from './firebase';
 
 export const Auth = ({ children }: any) => {
   const user = useSelector(selectUser);
-  console.log(user);
 
   useJudgeLogin();
-
-  //   useEffect(() => {
-  //     if (router.pathname.match('/users(/S)?')) {
-  //       if (!user.uid /*reduxのStateがfalseならば */) {
-  //         Router.push('/'); // signinページに飛ばす
-  //       }
-  //     }
-  //   }, [router.pathname]);
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -43,9 +32,8 @@ export const Auth = ({ children }: any) => {
   }, [dispatch]);
 
   useEffect(() => {
-    const { uid } = router.query as Params;
     if (user.uid) {
-      db.collection('users').doc(uid).set({
+      db.collection('users').doc(user.uid).set({
         avatar: user.photoUrl,
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         username: user.displayName,
@@ -53,7 +41,7 @@ export const Auth = ({ children }: any) => {
         twitterid: user.twitterUid,
       });
     }
-  }, []);
+  }, [user.uid]);
 
   return children;
 };
