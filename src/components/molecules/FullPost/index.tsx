3b12a73260e2342ca441dd9bc963d1b764daf41e
shuntatakemoto <rt80405@gmail.com>
2021-12-22 15:38:36 +0900

    fix: ブックマーク削除時の挙動の修正

--- src/components/molecules/FullPost/index.tsx
+++ src/components/molecules/FullPost/index.tsx
@@ -20,7 +20,6 @@ export type FullPostProps = {
 const FullPost: React.FC<FullPostProps> = (props) => {
   const emojiName = props.emojiname;
   const router = useRouter();
-  console.log(router.pathname);
 
   const deleteBookmark = () => {
     db.collection('users')
@@ -39,20 +38,23 @@ const FullPost: React.FC<FullPostProps> = (props) => {
   return (
     <div className=' rounded-lg shadow-xl overflow-hidden h-48 xl:h-60 m-4 xl:m-6'>
       {router.pathname.includes('bookmark') ? (
-        <Link href={`${props.listurl}`} passHref>
-          <div className='bg-gray-200'>
-            <button onClick={deleteBookmark} className='w-full h-8 grid justify-end items-center'>
+        <div className='bg-gray-200'>
+          <div className='grid justify-end items-center'>
+            <button onClick={deleteBookmark} className='h-8'>
               <HighlightOffIcon />
             </button>
-            <div className='h-28 xl:h-40 grid justify-items-center items-center '>
-              <Emoji emoji={emojiName} size={64} set='twitter' />
-            </div>
-
-            <div className='text-center h-12 xl:h-12 grid justify-items-center items-center'>
-              <p className='text-base'>{props.listname}</p>
-            </div>
           </div>
-        </Link>
+          <Link href={`${props.listurl}`} passHref>
+            <div>
+              <div className='h-28 xl:h-40 grid justify-items-center items-center'>
+                <Emoji emoji={emojiName} size={64} set='twitter' />
+              </div>
+              <div className='text-center h-12 xl:h-12 grid justify-items-center items-center'>
+                <p className='text-base'>{props.listname}</p>
+              </div>
+            </div>
+          </Link>
+        </div>
       ) : (
         <Link href={`/users/${props.uid}/lists/${props.listId}`} passHref>
           <div className='bg-gray-200'>
