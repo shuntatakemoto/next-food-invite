4540d8c49554dd36e71623f1a850bfe330b6779f
shuntatakemoto <rt80405@gmail.com>
2021-12-22 18:47:16 +0900

    feat: プロフィールにtwitterリンクを追加

--- src/components/molecules/Profile/index.tsx
+++ src/components/molecules/Profile/index.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { TwitterIcon } from 'react-share';
 import { useFullList } from '../../../hooks/useFullList';
+import { useUser } from '../../../hooks/useUser';
 import { selectUser } from '../../../store/user';
 
 export type ProfileProps = {
@@ -15,6 +18,8 @@ export const Profile: React.FC<ProfileProps> = ({ photoUrl, displayName }) => {
   const router = useRouter();
   const { uid } = router.query;
   const { posts } = useFullList();
+  const { userInfo } = useUser();
+
   return (
     <div className='mt-5 '>
       {uid == user.uid ? (
@@ -25,6 +30,11 @@ export const Profile: React.FC<ProfileProps> = ({ photoUrl, displayName }) => {
             className='w-40 rounded-3xl'
           />
           <p className='text-2xl font-bold'>{displayName ? displayName : 'NO NAME'}</p>
+          <Link href={`https://twitter.com/intent/user?user_id=${userInfo.twitterid}`} passHref>
+            <div>
+              <TwitterIcon size={32} round={true} />
+            </div>
+          </Link>
         </>
       ) : (
         <>
@@ -34,6 +44,11 @@ export const Profile: React.FC<ProfileProps> = ({ photoUrl, displayName }) => {
             className='w-40 rounded-3xl'
           />
           <p className='text-2xl font-bold'>{posts[0].username ? posts[0].username : 'NO NAME'}</p>
+          <Link href={`https://twitter.com/intent/user?user_id=${userInfo.twitterid}`} passHref>
+            <div>
+              <TwitterIcon size={32} round={true} />
+            </div>
+          </Link>
         </>
       )}
     </div>
