6c35299c4ad9c1747cba099c43632fc3c4b5d51a
shuntatakemoto <rt80405@gmail.com>
2021-12-28 02:56:16 +0900

    feat:  i18n対応

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   pageExtensions: ['page.tsx', 'page.ts'],
+  i18n: { locales: ['ja'], defaultLocale: 'ja' },
   reactStrictMode: true,
 };
