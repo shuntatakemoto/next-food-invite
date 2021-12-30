436afea63edb7a5f1f68b397cce5e34bafa7e764
shuntatakemoto <rt80405@gmail.com>
2021-12-30 12:12:52 +0900

    feat: Headlineのサイズを可変化

--- src/components/atoms/Headline/index.tsx
+++ src/components/atoms/Headline/index.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 export type HeadlineProps = {
   headline: string;
+  size?: 'small' | 'medium' | 'large';
 };
 
-export const Headline: React.FC<HeadlineProps> = ({ headline }) => {
-  return <div className='text-2xl font-bold text-center'>{headline}</div>;
+export const Headline: React.FC<HeadlineProps> = ({ headline, size = 'medium' }) => {
+  const sizeMode =
+    size === 'small'
+      ? 'text-lg'
+      : size === 'medium'
+      ? 'text-2xl'
+      : size === 'large'
+      ? 'text-3xl'
+      : '';
+  return <div className={`${sizeMode} font-bold text-center`}>{headline}</div>;
 };
