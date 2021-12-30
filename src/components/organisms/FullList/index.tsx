8f2b6aa3e3194c6e17401aaac1cb899e3fbae90b
shuntatakemoto <rt80405@gmail.com>
2021-12-30 12:14:23 +0900

    refactor: hooksの呼び出し場所をページに変更

--- src/components/organisms/FullList/index.tsx
+++ src/components/organisms/FullList/index.tsx
@@ -1,35 +1,39 @@
 import React from 'react';
-import { useFullList } from '../../../hooks/useFullList';
+import { Lists } from '../../../types/lists';
 import { Headline } from '../../atoms/Headline';
 import FullPost from '../../molecules/FullPost';
 
-const FullList: React.FC = () => {
-  const { uid, posts } = useFullList();
+export type FullListProps = {
+  posts: Lists;
+  uid: string;
+};
 
+const FullList: React.FC<FullListProps> = (props) => {
   return (
     <div>
-      <div className='py-5'>
+      <div className='py-4'>
         <Headline headline='マイリスト' />
       </div>
-
-      <div className='grid grid-cols-2 xl:grid-cols-4 text-center'>
-        {posts[0]?.id && (
-          <>
-            {posts.map((post) => (
-              <FullPost
-                key={post.id}
-                uid={uid}
-                listId={post.id}
-                listname={post.listname}
-                timestamp={post.timestamp}
-                username={post.username}
-                emojiname={post.emojiname}
-                isBookmarkPage={false}
-              />
-            ))}
-          </>
-        )}
-      </div>
+      {props.posts[0]?.id ? (
+        <div className='grid grid-cols-2 xl:grid-cols-4 text-center'>
+          {props.posts.map((post) => (
+            <FullPost
+              key={post.id}
+              uid={props.uid}
+              listId={post.id}
+              listname={post.listname}
+              timestamp={post.timestamp}
+              username={post.username}
+              emojiname={post.emojiname}
+              isBookmarkPage={false}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className='py-12'>
+          <Headline headline='まだリストがありません' size='small' />
+        </div>
+      )}
     </div>
   );
 };
