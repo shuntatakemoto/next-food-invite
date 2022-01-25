5c4ddb61d14ec7eccf01d422f0b7c9adb6403e06
shuntatakemoto <rt80405@gmail.com>
2022-01-25 13:55:54 +0900

    fix: compilerOptions.paths追加に伴う修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,22 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: [
     '../src/components/**/*.stories.mdx',
     '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+
+  addons: ['@storybook/addon-essentials', '@storybook/addon-links'],
+  babel: async (options) => ({
+    ...options,
+    plugins: [...options.plugins, require.resolve('@babel/plugin-transform-react-jsx')],
+  }),
+  webpackFinal: async (config) => {
+    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../src')];
+
+    config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin()];
+
+    return config;
+  },
 };
