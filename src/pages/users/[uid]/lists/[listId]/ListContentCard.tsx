3ffcd6518a7c352db5350c1396f6292a3b95eee2
shuntatakemoto <rt80405@gmail.com>
2022-02-02 17:24:44 +0900

    refactor: デザインの調整とnext/image化

--- src/pages/users/[uid]/lists/[listId]/ListContentCard.tsx
+++ src/pages/users/[uid]/lists/[listId]/ListContentCard.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
-/* eslint-disable @next/next/no-img-element */
 import firebase from 'firebase/app';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -20,17 +19,19 @@ const ListContentCard: React.FC<ListContentCardProps> = (props) => {
   const { uid, listId } = router.query;
 
   return (
-    <div className='overflow-hidden m-4 h-48 rounded-lg xl:m-6 xl:h-60'>
+    <div className='overflow-hidden m-4 rounded-lg md:m-6'>
       <Link href={`/users/${uid}/lists/${listId}/restaurant/${props.restaurantId}`} passHref>
         <div>
-          <div className='grid justify-items-center items-center h-36 bg-gray-200 xl:h-48'>
-            <img
+          <div className='grid justify-items-center items-center w-full bg-sub-color'>
+            <Image
               src={props.imageurl}
               alt='food-image'
-              className='object-cover w-full h-36 xl:h-48'
+              width={560}
+              height={560}
+              objectFit={'cover'}
             />
           </div>
-          <div className='grid justify-items-center items-center h-12 text-center bg-gray-200 xl:h-12'>
+          <div className='grid justify-items-center items-center py-2 text-center bg-gray-200 xl:py-4'>
             <p className='text-base'>{props.name}</p>
           </div>
         </div>
